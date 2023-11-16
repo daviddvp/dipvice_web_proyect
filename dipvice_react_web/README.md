8d46bc37c992a035d959fd7c967568fd535fbc04
David Del Valle <david@delvalledev.com>
2023-11-16 12:36:58 +0100

    Inicio del proyecto

---
+++ dipvice_react_web/README.md
@@ -0,0 +1,5 @@
+# Dipvice_web_proyect
+Portfolio of a photographer
+Created by David Del Valle david@delvalledev.com
+
+# ©2023_delvalledev.com
---
+++ dipvice_react_web/src/MyCompo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MyCompo = () => {
+	return (
+	<p>Este es mi primer componente</p>
+	);
+};
+
+export default MyCompo;
